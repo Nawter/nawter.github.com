a9d8f43236188fc145cb12269f49567f3948bdae
nawter <zaiddawoodissa@gmail.com>
2018-11-26 22:50:29 +0100

    new data to push

--- docs/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=/Users/zaidalissa/.rvm/gems/ruby-2.4.2/bin:/Users/zaidalissa/.rvm/gems/ruby-2.4.2@global/bin:/Users/zaidalissa/.rvm/rubies/ruby-2.4.2/bin:/Users/zaidalissa/.rvm/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-export MEDIUM_USERNAME=zaidalissa
-ruby -v
-gem install feedjira
-jekyll build
-aws s3 sync /Users/zaidalissa/Documents/Development/zaidalissa/_site s3://www.safacreations.net --delete
